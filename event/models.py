25febaaa218d0d71fcbdcf5818b9c1a17386a09c
Morgan Asare <ohenemorgan@gmail.com>
2024-06-12 20:39:57 +0000

    add event model

--- event/models.py
+++ event/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    """
+    A class for the About model
+    """
+    event_type = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
