ebed0529c9d269bb49a6f9e35250a8611f0b97e3
Morgan Asare <ohenemorgan@gmail.com>
2024-06-16 22:47:57 +0000

    add booking page

--- booking/urls.py
+++ booking/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.service_list, name='service_list'),
-    path('book/<int:service_id>/', views.create_booking, name='create_booking'),
-    path('bookings/', views.booking_list, name='booking_list'),
+    # path('', views.service_list, name='service_list'),
+    # path('book/<int:service_id>/', views.create_booking, name='create_booking'),
+    # path('', views.booking_list, name='booking_list'),
+    path('', views.create_booking, name='booking'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,34 +1,51 @@
 from django.shortcuts import render, redirect
+from django.views import generic
 from django.contrib.auth.decorators import login_required
 from .models import Service, Booking
 from .forms import BookingForm
 
 
 # Create your views here.
-
-def service_list(request):
-    services = Service.objects.all()
-    return render(request, 'booking/service_list.html', {'services':services} )
-
-@login_required
-def create_booking(request, service_id):
-    service = Service.objects.get(id=service_id)
+def create_booking(request):
+    bookings = Booking.objects.all()
     if request.method == 'POST':
         form = BookingForm(request.POST)
         if form.is_valid():
-            booking = form.save(commit=False)
-            booking.user = request.user
-            booking.service = service
-            booking.save()
-            return redirect('booking_list')
+            form.save()
+            return redirect('Success')
     else:
-        form = BookingForm(initial={'service': service})
-    return render(request, 'bookings/create_booking.html', {'form': form, 'service': service})
+        form = BookingForm()
+    
+    return render(request, 'booking/booking.html', {'bookings': bookings})
 
 
-@login_required
-def booking_list(request):
-    bookings = Booking.objects.filter(user=request.user)
-    return render(request, 'bookings/booking_list.html', {'bookings': bookings})
+
+
+
+
+# def service_list(request):
+#     services = Service.objects.all()
+#     return render(request, 'booking/service_list.html', {'services':services} )
+
+# @login_required
+# # def create_booking(request, service_id):
+#     # service = Service.objects.get(id=service_id)
+# if request.method == 'POST':
+#     form = BookingForm(request.POST)
+#     if form.is_valid():
+#         booking = form.save(commit=False)
+#         booking.user = request.user
+#         booking.service = service
+#         booking.save()
+#         return redirect('booking_list')
+# else:
+#     form = BookingForm(initial={'service': service})
+# return render(request, 'booking/booking.html', {'form': form, 'service': service})
+
+
+# @login_required
+# def booking_list(request):
+#     bookings = Booking.objects.filter(user=request.user)
+#     return render(request, 'booking/booking_list.html', {'booking': booking})
 
 
