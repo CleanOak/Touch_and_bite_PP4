a6ac5905463be85abacdc222cf071de2f299f6f9
Morgan Asare <ohenemorgan@gmail.com>
2024-06-19 16:40:24 +0000

    add contact form to about page

---
+++ about/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from phonenumber_field.formfields import PhoneNumberField
+from .models import Contact
+
+
+class ContactForm(forms.Form):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Submit'))
+
+    
+    phone = PhoneNumberField(widget=forms.TextInput(
+        attrs={'placeholder': ('+44792002022')}
+    ))
+
+    class Meta:
+        model = Contact
+        fields = ['name', 'email', 'phone', 'message']
