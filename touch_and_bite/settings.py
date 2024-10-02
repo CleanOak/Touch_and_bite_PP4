61e2a00b3e491ad15482de0074ffcf48923b7c5a
Morgan Asare <ohenemorgan@gmail.com>
2024-10-02 09:50:46 +0000

    import cloudinary in settings

--- touch_and_bite/settings.py
+++ touch_and_bite/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 import dj_database_url
+import cloudinary
 if os.path.isfile('env.py'):
     import env
 
@@ -176,9 +177,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 # Default primary key field type
