129ec05697caf20f55513d45469b4e6fe331ba97
Morgan Asare <ohenemorgan@gmail.com>
2024-12-24 14:15:11 +0000

    change debug to false

--- touch_and_bite/settings.py
+++ touch_and_bite/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-cleanoak-touchandbitepp-4mdp1il4np8.ws.codeinstitute-ide.net',
